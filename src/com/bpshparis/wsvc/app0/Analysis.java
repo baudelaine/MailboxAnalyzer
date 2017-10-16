4d982bf065342fd242ddbb1c9f5f690a1abd57c6
root <root@tp054721>
2017-10-16 21:55:13 +0200

    init

---
+++ src/com/bpshparis/wsvc/app0/Analysis.java
@@ -0,0 +1,53 @@
+package com.bpshparis.wsvc.app0;
+
+import java.util.Map;
+
+public class Analysis {
+
+	private Map<String, Object> nlu;
+	private Map<String, Object> ta;
+	private Map<String, Object> d;
+	private Map<String, Object> vr;
+	private Map<String, Object> fr;
+	private Map<String, Object> tr;
+	
+	public Map<String, Object> getNlu() {
+		return nlu;
+	}
+	public void setNlu(Map<String, Object> nlu) {
+		this.nlu = nlu;
+	}
+	public Map<String, Object> getTa() {
+		return ta;
+	}
+	public void setTa(Map<String, Object> ta) {
+		this.ta = ta;
+	}
+	public Map<String, Object> getD() {
+		return d;
+	}
+	public void setD(Map<String, Object> d) {
+		this.d = d;
+	}
+	public Map<String, Object> getVr() {
+		return vr;
+	}
+	public void setVr(Map<String, Object> vr) {
+		this.vr = vr;
+	}
+	public Map<String, Object> getFr() {
+		return fr;
+	}
+	public void setFr(Map<String, Object> fr) {
+		this.fr = fr;
+	}
+	public Map<String, Object> getTr() {
+		return tr;
+	}
+	public void setTr(Map<String, Object> tr) {
+		this.tr = tr;
+	}
+
+	
+	
+}
